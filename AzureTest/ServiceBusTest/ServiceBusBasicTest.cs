00ad1b6237e0d3375bf460ed022bfa6ad3286297
Amar Kumar Dubedy <amdubedy@microsoft.com>
2017-04-24 17:32:33 -0700

    First commit to Azure

---
+++ AzureTest/ServiceBusTest/ServiceBusBasicTest.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ServiceBusBasicTest.cs" company="Microsoft">Copyright (c) Microsoft. All rights reserved.</copyright>
+namespace AzureTest.ServiceBusTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ServiceBusBasicTest
+    {
+        [TestMethod]
+        public void BasicServiceBusTest()
+        {
+
+        }
+
+    }
+}
