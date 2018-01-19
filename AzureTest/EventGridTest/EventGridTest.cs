a8530ea07c489ee427b08dbf0a076b263d382762
Amar Kumar Dubedy <amdubedy@microsoft.com>
2018-01-19 11:35:21 -0800

    checking in temp changes

---
+++ AzureTest/EventGridTest/EventGridTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureTest.EventGridTest
+{
+    [TestClass]
+    public class EventGridTest
+    {
+
+    }
+}
--- AzureTest/Storage/AzureSearchTest.cs
+++ AzureTest/Storage/AzureSearchTest.cs
@@ -1,32 +1,26 @@
-﻿using System;
+﻿using System.Configuration;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AzureTest.Storage
 {
-    using System.Collections.Generic;
-    using System.Configuration;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Microsoft.Azure.Search;
-    using Microsoft.Azure.Search.Models;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
     [TestClass]
     public class AzureSearchTest
     {
         [TestMethod]
         public void TestIndexCreation()
         {
-            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string apiKey = ConfigurationManager.AppSettings["SearchKey"];
+            var searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            var apiKey = ConfigurationManager.AppSettings["SearchKey"];
             var searchServiceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
-            if(searchServiceClient.Indexes.Exists("hotels"))
+            if (searchServiceClient.Indexes.Exists("hotels"))
             {
                 searchServiceClient.Indexes.Delete("hotels");
                 Assert.IsTrue(searchServiceClient.Indexes.Exists("hotels") == false, "Indexes not deleted");
             }
 
-            var indexDefinition = new Index()
+            var indexDefinition = new Index
             {
                 Name = "hotels",
                 Fields = FieldBuilder.BuildForType<Hotel>()
@@ -39,8 +33,8 @@ namespace AzureTest.Storage
         [ClassCleanup]
         public static void Cleanup()
         {
-            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string apiKey = ConfigurationManager.AppSettings["SearchKey"];
+            var searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            var apiKey = ConfigurationManager.AppSettings["SearchKey"];
             var searchServiceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
             if (searchServiceClient.Indexes.Exists("hotels"))
             {
