590303d2f8bf9f2a7cb0f0970c642c9d960bde27
Amar Kumar Dubedy <amarkrdubedy@gmail.com>
2018-01-22 12:00:31 -0800

    Azure management Fluent APIs are working

---
+++ AzureManagerTest/AzureHelper.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
+
+
+namespace AzureManagerTest
+{
+    public static class AzureHelper
+    {
+        public static IResourceGroup CreateResourceGroupIfNotExists(IAzure azure, string groupName, Region region)
+        {
+            var resourceGroups = azure.ResourceGroups.List();
+            if (resourceGroups.Any(rg => rg.Name.Equals(groupName)))
+            {
+                Console.WriteLine("Resource Group already exists");
+                return resourceGroups.SingleOrDefault(rg => rg.Name.Equals(groupName));
+            }
+
+            Console.WriteLine("Creating resource group...");
+            var resourceGroup = azure.ResourceGroups.Define(groupName)
+                .WithRegion(region)
+                .Create();
+
+            return resourceGroup;
+        }
+
+        public static IAvailabilitySet CreateAvailabilitySetIfNotExists(IAzure azure, string groupName, Region location, string availabiltiySetName)
+        {
+            var availabilitySets = azure.AvailabilitySets.List();
+            if (availabilitySets.Any(av => av.Name.Equals(availabiltiySetName)))
+            {
+                Console.WriteLine("Availability Set already exists");
+                return availabilitySets.SingleOrDefault(av => av.Name.Equals(availabiltiySetName));
+            }
+
+            Console.WriteLine("Creating availability set...");
+            var availabilitySet = azure.AvailabilitySets.Define(availabiltiySetName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithSku(AvailabilitySetSkuTypes.Managed)
+                .Create();
+
+            return availabilitySet;
+        }
+
+        public static IPublicIPAddress CreatePublicIpIfNotExists(IAzure azure, string groupName, Region location, string publicIpAddressName)
+        {
+            var publicIpAddresses = azure.PublicIPAddresses.List();
+            if (publicIpAddresses.Any(pip => pip.Name.Equals(publicIpAddressName)))
+            {
+                Console.WriteLine("Public Ip Address already exists");
+                return publicIpAddresses.SingleOrDefault(pip => pip.Name.Equals(publicIpAddressName));
+            }
+
+            Console.WriteLine("Creating public IP address...");
+            var publicIPAddress = azure.PublicIPAddresses.Define(publicIpAddressName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithDynamicIP()
+                .Create();
+
+            return publicIPAddress;
+        }
+
+        public static INetwork CreateVirtualNetworkIfNotExists(IAzure azure, string groupName, Region location, string vNetName)
+        {
+            var vNetList = azure.Networks.List();
+            if (vNetList.Any(vnet => vnet.Name.Equals(vNetName)))
+            {
+                Console.WriteLine("Virtual Network already exists");
+                return vNetList.SingleOrDefault(vnet => vnet.Name.Equals(vNetName));
+            }
+
+            Console.WriteLine("Creating virtual network...");
+            var network = azure.Networks.Define(vNetName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithAddressSpace("10.0.0.0/16")
+                .WithSubnet("mySubnet", "10.0.0.0/24")
+                .Create();
+
+            return network;
+        }
+
+        public static INetworkInterface CreateNetworkInterfaceIfNotExists(IAzure azure, string groupName, Region location, INetwork network, IPublicIPAddress publicIPAddress, string subnetName, string networkInterfaceName)
+        {
+            var networkInterfaces = azure.NetworkInterfaces.List();
+            if (networkInterfaces.Any(ni => ni.Name.Equals(networkInterfaceName)))
+            {
+                Console.WriteLine("Network Interfaces already exists");
+                return networkInterfaces.SingleOrDefault(ni => ni.Name.Equals(networkInterfaceName));
+            }
+
+            Console.WriteLine("Creating network interface...");
+            var networkInterface = azure.NetworkInterfaces.Define(networkInterfaceName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithExistingPrimaryNetwork(network)
+                .WithSubnet(subnetName)
+                .WithPrimaryPrivateIPAddressDynamic()
+                .WithExistingPrimaryPublicIPAddress(publicIPAddress)
+                .Create();
+
+            return networkInterface;
+        }
+
+        public static bool TestCredentialsAreValid(IAzure azure)
+        {
+            return azure.Subscriptions.List().Count() == 1;
+        }
+
+        public static IVirtualMachine CreateVirtualMachineIfNotExists(IAzure azure, string groupName, Region location, INetworkInterface networkInterface, IAvailabilitySet availabilitySet, string vmName, string userName, string password)
+        {
+            var vmList = azure.VirtualMachines.List();
+            if (vmList.Any(vm => vm.Name.Equals(vmName)))
+            {
+                Console.WriteLine("VM already exists");
+                return vmList.SingleOrDefault(pip => pip.Name.Equals(vmName));
+            }
+
+            Console.WriteLine("Creating virtual machine...");
+            var virtualMachine = azure.VirtualMachines.Define(vmName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithExistingPrimaryNetworkInterface(networkInterface)
+                .WithLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-Datacenter")
+                .WithAdminUsername(userName)
+                .WithAdminPassword(password)
+                .WithComputerName(vmName)
+                .WithExistingAvailabilitySet(availabilitySet)
+                .WithSize(VirtualMachineSizeTypes.StandardDS1)
+                .Create();
+
+            return virtualMachine;
+        }
+    }
+}
--- AzureManagerTest/Program.cs
+++ AzureManagerTest/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using System.Threading;
 
 namespace AzureManagerTest
 {
@@ -14,26 +17,138 @@ namespace AzureManagerTest
         private static string clientId = "f2a7e58f-681c-4416-83cb-409d0b5dfb01";
         private static string clientSecret = "AmarTest123!";
         private static string tenantId = "a8e776e2-1710-4ed5-8c2f-1a49f83724ec";
+        private static string resourceGroupName = "myTestResourceGroup";
+        private static string availabilitySetName = "myAvailabilitySet";
+        private static string publicIpAddressName = "myPublicIpAddress";
+        private static string subnetName = "mySubnet";
+        private static string networkInterfaceName = "myNetworkInterface";
+        private static string vNetName = "myVNet";
+        private static string vmName = "amarVmName";
+        private static string userName = "amar";
+        private static string password = "AmarTest123!";
+        private static Region location = Region.USWest;
 
         static void Main(string[] args)
         {
             var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
             var azure = Azure.Configure().Authenticate(credentials).WithDefaultSubscription();
-            TestCredentialsAreValid(azure);
-        }
+            Console.WriteLine($"Credentials are valid: {AzureHelper.TestCredentialsAreValid(azure)}");
 
-        static bool TestCredentialsAreValid(IAzure azure)
-        {
-            return azure.Subscriptions.List().Count() == 1;
-        }
 
-        static void TestVmCrudAndRestart(IAzure azure)
-        {
-            var vmList = azure.VirtualMachines.List();
-            foreach (var vm in vmList)
+            
+
+            var resourceGroup = AzureHelper.CreateResourceGroupIfNotExists(azure, resourceGroupName, location);
+
+            var availabilitySet = AzureHelper.CreateAvailabilitySetIfNotExists(azure, resourceGroupName, location, availabilitySetName);
+
+            var publicIpAddress = AzureHelper.CreatePublicIpIfNotExists(azure, resourceGroupName, location, publicIpAddressName);
+
+            var virtualNetwork = AzureHelper.CreateVirtualNetworkIfNotExists(azure, resourceGroupName, location, vNetName);
+
+            var networkInterface = AzureHelper.CreateNetworkInterfaceIfNotExists(azure, resourceGroupName, location, virtualNetwork, publicIpAddress, subnetName, networkInterfaceName);
+
+            var virtualMachine = AzureHelper.CreateVirtualMachineIfNotExists(azure, resourceGroupName, location, networkInterface, availabilitySet, vmName, userName, password);
+
+            PrintVmStatus(virtualMachine);
+
+            virtualMachine.RestartAsync();
+
+            int i = 0;
+            while (i < 2)
             {
-                Console.WriteLine("VM Name: {0}", vm.Name);
+                Console.WriteLine($"Counter: {i}");
+                PrintInstanceStatus(virtualMachine);
+                Thread.Sleep(10000);
+                i++;
+            }
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+        private static void PrintInstanceStatus(IVirtualMachine vm)
+        {
+            Console.WriteLine("VM general status");
+            Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
+            Console.WriteLine("  id: " + vm.Id);
+            Console.WriteLine("  name: " + vm.Name);
+            Console.WriteLine("  type: " + vm.Type);
+            Console.WriteLine("  location: " + vm.Region);
+            Console.WriteLine("VM instance status");
+            foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
+            {
+                Console.WriteLine("  code: " + stat.Code);
+                Console.WriteLine("  level: " + stat.Level);
+                Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
             }
         }
+
+        static void PrintVmStatus(IVirtualMachine vm)
+        {
+            Console.WriteLine("Getting information about the virtual machine...");
+            Console.WriteLine("hardwareProfile");
+            Console.WriteLine("   vmSize: " + vm.Size);
+            Console.WriteLine("storageProfile");
+            Console.WriteLine("  imageReference");
+            Console.WriteLine("    publisher: " + vm.StorageProfile.ImageReference.Publisher);
+            Console.WriteLine("    offer: " + vm.StorageProfile.ImageReference.Offer);
+            Console.WriteLine("    sku: " + vm.StorageProfile.ImageReference.Sku);
+            Console.WriteLine("    version: " + vm.StorageProfile.ImageReference.Version);
+            Console.WriteLine("  osDisk");
+            Console.WriteLine("    osType: " + vm.StorageProfile.OsDisk.OsType);
+            Console.WriteLine("    name: " + vm.StorageProfile.OsDisk.Name);
+            Console.WriteLine("    createOption: " + vm.StorageProfile.OsDisk.CreateOption);
+            Console.WriteLine("    caching: " + vm.StorageProfile.OsDisk.Caching);
+            Console.WriteLine("osProfile");
+            Console.WriteLine("  computerName: " + vm.OSProfile.ComputerName);
+            Console.WriteLine("  adminUsername: " + vm.OSProfile.AdminUsername);
+            Console.WriteLine("  provisionVMAgent: " + vm.OSProfile.WindowsConfiguration.ProvisionVMAgent.Value);
+            Console.WriteLine("  enableAutomaticUpdates: " + vm.OSProfile.WindowsConfiguration.EnableAutomaticUpdates.Value);
+            Console.WriteLine("networkProfile");
+            foreach (string nicId in vm.NetworkInterfaceIds)
+            {
+                Console.WriteLine("  networkInterface id: " + nicId);
+            }
+            Console.WriteLine("vmAgent");
+            Console.WriteLine("  vmAgentVersion" + vm.InstanceView.VmAgent.VmAgentVersion);
+            Console.WriteLine("    statuses");
+            foreach (InstanceViewStatus stat in vm.InstanceView.VmAgent.Statuses)
+            {
+                Console.WriteLine("    code: " + stat.Code);
+                Console.WriteLine("    level: " + stat.Level);
+                Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
+                Console.WriteLine("    message: " + stat.Message);
+                Console.WriteLine("    time: " + stat.Time);
+            }
+            Console.WriteLine("disks");
+            foreach (DiskInstanceView disk in vm.InstanceView.Disks)
+            {
+                Console.WriteLine("  name: " + disk.Name);
+                Console.WriteLine("  statuses");
+                foreach (InstanceViewStatus stat in disk.Statuses)
+                {
+                    Console.WriteLine("    code: " + stat.Code);
+                    Console.WriteLine("    level: " + stat.Level);
+                    Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
+                    Console.WriteLine("    time: " + stat.Time);
+                }
+            }
+            Console.WriteLine("VM general status");
+            Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
+            Console.WriteLine("  id: " + vm.Id);
+            Console.WriteLine("  name: " + vm.Name);
+            Console.WriteLine("  type: " + vm.Type);
+            Console.WriteLine("  location: " + vm.Region);
+            Console.WriteLine("VM instance status");
+            foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
+            {
+                Console.WriteLine("  code: " + stat.Code);
+                Console.WriteLine("  level: " + stat.Level);
+                Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
+            }
+            
+        }
+
+        
     }
 }
