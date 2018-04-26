739f8b2fca6181518078f357a01386c96e42744c
Amar Kumar Dubedy <amdubedy@microsoft.com>
2018-04-26 16:23:32 -0700

    Added sample cloud service + updated iothubgetstarted

--- AzureManagerTest/Program.cs
+++ AzureManagerTest/Program.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.Management.Fluent;
+//using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.Compute.Fluent;
-using Microsoft.Azure.Management.Compute.Fluent.Models;
+//using Microsoft.Azure.Management.Compute.Fluent;
+//using Microsoft.Azure.Management.Compute.Fluent.Models;
 using System.Threading;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Rest.Azure.Authentication;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Compute.Models;
 
 namespace AzureManagerTest
 {
     class Program
     {
         private static string clientId = "f2a7e58f-681c-4416-83cb-409d0b5dfb01";
-        private static string clientSecret = "AmarTest123!";
-        private static string tenantId = "a8e776e2-1710-4ed5-8c2f-1a49f83724ec";
+        private static string clientSecret = "AmarTest123!"; //yE0A0FGQaGI7D8pebMJNYpSzxMVqv19LWkBFXlGGI/Y=
+        private static string tenantId = "a8e776e2-1710-4ed5-8c2f-1a49f83724ec"; 
         private static string resourceGroupName = "myTestResourceGroup";
         private static string availabilitySetName = "myAvailabilitySet";
         private static string publicIpAddressName = "myPublicIpAddress";
@@ -30,124 +37,139 @@ namespace AzureManagerTest
 
         static void Main(string[] args)
         {
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
-            var azure = Azure.Configure().Authenticate(credentials).WithDefaultSubscription();
-            Console.WriteLine($"Credentials are valid: {AzureHelper.TestCredentialsAreValid(azure)}");
+            var authenticationContext =
+                new AuthenticationContext("https://login.windows.net/" + tenantId);
+            var credential = new ClientCredential(clientId: clientId, clientSecret: clientSecret);
 
+            var result = authenticationContext.AcquireToken(resource: "https://management.core.windows.net/",
+                clientCredential: credential);
+            //ComputeManagementClient computeClient =
+            //    new ComputeManagementClient(new TokenCloudCredentials("eb0f2b9b-b44c-4019-9a47-6117d9b6cdfa", result.AccessToken));
 
+            //var operationStatus = computeClient.Deployments.RebootRoleInstanceByDeploymentSlotAsync("sampleworkerroleamar", DeploymentSlot.Production, "SampleWorkerRole_IN_0").GetAwaiter().GetResult();
             
-
-            var resourceGroup = AzureHelper.CreateResourceGroupIfNotExists(azure, resourceGroupName, location);
-
-            var availabilitySet = AzureHelper.CreateAvailabilitySetIfNotExists(azure, resourceGroupName, location, availabilitySetName);
-
-            var publicIpAddress = AzureHelper.CreatePublicIpIfNotExists(azure, resourceGroupName, location, publicIpAddressName);
-
-            var virtualNetwork = AzureHelper.CreateVirtualNetworkIfNotExists(azure, resourceGroupName, location, vNetName);
-
-            var networkInterface = AzureHelper.CreateNetworkInterfaceIfNotExists(azure, resourceGroupName, location, virtualNetwork, publicIpAddress, subnetName, networkInterfaceName);
-
-            var virtualMachine = AzureHelper.CreateVirtualMachineIfNotExists(azure, resourceGroupName, location, networkInterface, availabilitySet, vmName, userName, password);
-
-            PrintVmStatus(virtualMachine);
-
-            virtualMachine.RestartAsync();
-
-            int i = 0;
-            while (i < 2)
-            {
-                Console.WriteLine($"Counter: {i}");
-                PrintInstanceStatus(virtualMachine);
-                Thread.Sleep(10000);
-                i++;
-            }
-
+            
             Console.WriteLine("Press enter to continue...");
             Console.ReadLine();
         }
 
-        private static void PrintInstanceStatus(IVirtualMachine vm)
-        {
-            Console.WriteLine("VM general status");
-            Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
-            Console.WriteLine("  id: " + vm.Id);
-            Console.WriteLine("  name: " + vm.Name);
-            Console.WriteLine("  type: " + vm.Type);
-            Console.WriteLine("  location: " + vm.Region);
-            Console.WriteLine("VM instance status");
-            foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
-            {
-                Console.WriteLine("  code: " + stat.Code);
-                Console.WriteLine("  level: " + stat.Level);
-                Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
-            }
-        }
+        //private static void RunBasicAzureManagementTest()
+        //{
+        //    var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
+        //    var azure = Azure.Configure().Authenticate(credentials).WithDefaultSubscription();
+        //    Console.WriteLine($"Credentials are valid: {AzureHelper.TestCredentialsAreValid(azure)}");
 
-        static void PrintVmStatus(IVirtualMachine vm)
-        {
-            Console.WriteLine("Getting information about the virtual machine...");
-            Console.WriteLine("hardwareProfile");
-            Console.WriteLine("   vmSize: " + vm.Size);
-            Console.WriteLine("storageProfile");
-            Console.WriteLine("  imageReference");
-            Console.WriteLine("    publisher: " + vm.StorageProfile.ImageReference.Publisher);
-            Console.WriteLine("    offer: " + vm.StorageProfile.ImageReference.Offer);
-            Console.WriteLine("    sku: " + vm.StorageProfile.ImageReference.Sku);
-            Console.WriteLine("    version: " + vm.StorageProfile.ImageReference.Version);
-            Console.WriteLine("  osDisk");
-            Console.WriteLine("    osType: " + vm.StorageProfile.OsDisk.OsType);
-            Console.WriteLine("    name: " + vm.StorageProfile.OsDisk.Name);
-            Console.WriteLine("    createOption: " + vm.StorageProfile.OsDisk.CreateOption);
-            Console.WriteLine("    caching: " + vm.StorageProfile.OsDisk.Caching);
-            Console.WriteLine("osProfile");
-            Console.WriteLine("  computerName: " + vm.OSProfile.ComputerName);
-            Console.WriteLine("  adminUsername: " + vm.OSProfile.AdminUsername);
-            Console.WriteLine("  provisionVMAgent: " + vm.OSProfile.WindowsConfiguration.ProvisionVMAgent.Value);
-            Console.WriteLine("  enableAutomaticUpdates: " + vm.OSProfile.WindowsConfiguration.EnableAutomaticUpdates.Value);
-            Console.WriteLine("networkProfile");
-            foreach (string nicId in vm.NetworkInterfaceIds)
-            {
-                Console.WriteLine("  networkInterface id: " + nicId);
-            }
-            Console.WriteLine("vmAgent");
-            Console.WriteLine("  vmAgentVersion" + vm.InstanceView.VmAgent.VmAgentVersion);
-            Console.WriteLine("    statuses");
-            foreach (InstanceViewStatus stat in vm.InstanceView.VmAgent.Statuses)
-            {
-                Console.WriteLine("    code: " + stat.Code);
-                Console.WriteLine("    level: " + stat.Level);
-                Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
-                Console.WriteLine("    message: " + stat.Message);
-                Console.WriteLine("    time: " + stat.Time);
-            }
-            Console.WriteLine("disks");
-            foreach (DiskInstanceView disk in vm.InstanceView.Disks)
-            {
-                Console.WriteLine("  name: " + disk.Name);
-                Console.WriteLine("  statuses");
-                foreach (InstanceViewStatus stat in disk.Statuses)
-                {
-                    Console.WriteLine("    code: " + stat.Code);
-                    Console.WriteLine("    level: " + stat.Level);
-                    Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
-                    Console.WriteLine("    time: " + stat.Time);
-                }
-            }
-            Console.WriteLine("VM general status");
-            Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
-            Console.WriteLine("  id: " + vm.Id);
-            Console.WriteLine("  name: " + vm.Name);
-            Console.WriteLine("  type: " + vm.Type);
-            Console.WriteLine("  location: " + vm.Region);
-            Console.WriteLine("VM instance status");
-            foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
-            {
-                Console.WriteLine("  code: " + stat.Code);
-                Console.WriteLine("  level: " + stat.Level);
-                Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
-            }
+
+
+
+        //    var resourceGroup = AzureHelper.CreateResourceGroupIfNotExists(azure, resourceGroupName, location);
+
+        //    var availabilitySet = AzureHelper.CreateAvailabilitySetIfNotExists(azure, resourceGroupName, location, availabilitySetName);
+
+        //    var publicIpAddress = AzureHelper.CreatePublicIpIfNotExists(azure, resourceGroupName, location, publicIpAddressName);
+
+        //    var virtualNetwork = AzureHelper.CreateVirtualNetworkIfNotExists(azure, resourceGroupName, location, vNetName);
+
+        //    var networkInterface = AzureHelper.CreateNetworkInterfaceIfNotExists(azure, resourceGroupName, location, virtualNetwork, publicIpAddress, subnetName, networkInterfaceName);
+
+        //    var virtualMachine = AzureHelper.CreateVirtualMachineIfNotExists(azure, resourceGroupName, location, networkInterface, availabilitySet, vmName, userName, password);
+
+        //    PrintVmStatus(virtualMachine);
+
+        //    virtualMachine.RestartAsync();
+
+        //    int i = 0;
+        //    while (i < 2)
+        //    {
+        //        Console.WriteLine($"Counter: {i}");
+        //        PrintInstanceStatus(virtualMachine);
+        //        Thread.Sleep(10000);
+        //        i++;
+        //    }
+        //}
+
+        //private static void PrintInstanceStatus(IVirtualMachine vm)
+        //{
+        //    Console.WriteLine("VM general status");
+        //    Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
+        //    Console.WriteLine("  id: " + vm.Id);
+        //    Console.WriteLine("  name: " + vm.Name);
+        //    Console.WriteLine("  type: " + vm.Type);
+        //    Console.WriteLine("  location: " + vm.Region);
+        //    Console.WriteLine("VM instance status");
+        //    foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
+        //    {
+        //        Console.WriteLine("  code: " + stat.Code);
+        //        Console.WriteLine("  level: " + stat.Level);
+        //        Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
+        //    }
+        //}
+
+        //static void PrintVmStatus(IVirtualMachine vm)
+        //{
+        //    Console.WriteLine("Getting information about the virtual machine...");
+        //    Console.WriteLine("hardwareProfile");
+        //    Console.WriteLine("   vmSize: " + vm.Size);
+        //    Console.WriteLine("storageProfile");
+        //    Console.WriteLine("  imageReference");
+        //    Console.WriteLine("    publisher: " + vm.StorageProfile.ImageReference.Publisher);
+        //    Console.WriteLine("    offer: " + vm.StorageProfile.ImageReference.Offer);
+        //    Console.WriteLine("    sku: " + vm.StorageProfile.ImageReference.Sku);
+        //    Console.WriteLine("    version: " + vm.StorageProfile.ImageReference.Version);
+        //    Console.WriteLine("  osDisk");
+        //    Console.WriteLine("    osType: " + vm.StorageProfile.OsDisk.OsType);
+        //    Console.WriteLine("    name: " + vm.StorageProfile.OsDisk.Name);
+        //    Console.WriteLine("    createOption: " + vm.StorageProfile.OsDisk.CreateOption);
+        //    Console.WriteLine("    caching: " + vm.StorageProfile.OsDisk.Caching);
+        //    Console.WriteLine("osProfile");
+        //    Console.WriteLine("  computerName: " + vm.OSProfile.ComputerName);
+        //    Console.WriteLine("  adminUsername: " + vm.OSProfile.AdminUsername);
+        //    Console.WriteLine("  provisionVMAgent: " + vm.OSProfile.WindowsConfiguration.ProvisionVMAgent.Value);
+        //    Console.WriteLine("  enableAutomaticUpdates: " + vm.OSProfile.WindowsConfiguration.EnableAutomaticUpdates.Value);
+        //    Console.WriteLine("networkProfile");
+        //    foreach (string nicId in vm.NetworkInterfaceIds)
+        //    {
+        //        Console.WriteLine("  networkInterface id: " + nicId);
+        //    }
+        //    Console.WriteLine("vmAgent");
+        //    Console.WriteLine("  vmAgentVersion" + vm.InstanceView.VmAgent.VmAgentVersion);
+        //    Console.WriteLine("    statuses");
+        //    foreach (InstanceViewStatus stat in vm.InstanceView.VmAgent.Statuses)
+        //    {
+        //        Console.WriteLine("    code: " + stat.Code);
+        //        Console.WriteLine("    level: " + stat.Level);
+        //        Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
+        //        Console.WriteLine("    message: " + stat.Message);
+        //        Console.WriteLine("    time: " + stat.Time);
+        //    }
+        //    Console.WriteLine("disks");
+        //    foreach (DiskInstanceView disk in vm.InstanceView.Disks)
+        //    {
+        //        Console.WriteLine("  name: " + disk.Name);
+        //        Console.WriteLine("  statuses");
+        //        foreach (InstanceViewStatus stat in disk.Statuses)
+        //        {
+        //            Console.WriteLine("    code: " + stat.Code);
+        //            Console.WriteLine("    level: " + stat.Level);
+        //            Console.WriteLine("    displayStatus: " + stat.DisplayStatus);
+        //            Console.WriteLine("    time: " + stat.Time);
+        //        }
+        //    }
+        //    Console.WriteLine("VM general status");
+        //    Console.WriteLine("  provisioningStatus: " + vm.ProvisioningState);
+        //    Console.WriteLine("  id: " + vm.Id);
+        //    Console.WriteLine("  name: " + vm.Name);
+        //    Console.WriteLine("  type: " + vm.Type);
+        //    Console.WriteLine("  location: " + vm.Region);
+        //    Console.WriteLine("VM instance status");
+        //    foreach (InstanceViewStatus stat in vm.InstanceView.Statuses)
+        //    {
+        //        Console.WriteLine("  code: " + stat.Code);
+        //        Console.WriteLine("  level: " + stat.Level);
+        //        Console.WriteLine("  displayStatus: " + stat.DisplayStatus);
+        //    }
             
-        }
+        //}
 
         
     }
--- IotHubGetStarted/SendCloudToDevice/Program.cs
+++ IotHubGetStarted/SendCloudToDevice/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Azure.Devices;
@@ -11,42 +12,76 @@ namespace SendCloudToDevice
     class Program
     {
         static ServiceClient serviceClient;
-        static string connectionString = "HostName=testamar.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Fcqxr60RiPqitTZH6dCThuB5yk8TFg5c2Kz3wTCUkDU=";
+        static string connectionString = "HostName=demobasichub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=UY4B/i5RhGdRH1mEhMUdjIiGjYhxi69L+5jYmhQZiTs=";
+        private static string deviceId = "myFirstDevice";
+        private static object _lock = new object();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Send Cloud-to-Device message\n");
+            Thread.Sleep(15000);
             serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
             ReceiveFeedbackAsync();
 
-            Console.WriteLine("Press any key to send a C2D message.");
-            Console.ReadLine();
-            SendCloudToDeviceMessageAsync().Wait();
+            //Console.WriteLine("Press any key to send a C2D message.");
+            //Console.ReadLine();
+            SendCloudToDeviceMessageAsync();
             Console.ReadLine();
         }
 
-        private async static Task SendCloudToDeviceMessageAsync()
+        private static async void SendCloudToDeviceMessageAsync()
         {
-            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
-            commandMessage.Ack = DeliveryAcknowledgement.Full;
-            await serviceClient.SendAsync("myFirstDevice", commandMessage);
+            while (true)
+            {
+                var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."))
+                {
+                    Ack = DeliveryAcknowledgement.Full
+                };
+                try
+                {
+                    await serviceClient.SendAsync(deviceId, commandMessage);
+                    Print("Sending Cloud-to-Device message successfull", ConsoleColor.Yellow);
+                }
+                catch (Exception ex)
+                {
+                    Print($"Exception thrown while sending message {ex.Message}\n", ConsoleColor.White);
+                }
+
+                await Task.Delay(3000);
+            }
         }
 
-        private async static void ReceiveFeedbackAsync()
+        private static async void ReceiveFeedbackAsync()
         {
             var feedbackReceiver = serviceClient.GetFeedbackReceiver();
 
             Console.WriteLine("\nReceiving c2d feedback from service");
             while (true)
             {
-                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
-                if (feedbackBatch == null) continue;
+                try
+                {
+                    var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                    if (feedbackBatch == null) continue;
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                    Print($"Received feedback: " + string.Join(",", feedbackBatch.Records.Select(f => f.StatusCode)), ConsoleColor.Green);
+
+                    await feedbackReceiver.CompleteAsync(feedbackBatch);
+                }
+                catch (Exception ex) 
+                {
+                    Print($"Exception thrown while receiving feedback {ex.Message}\n", ConsoleColor.White);
+                    await Task.Delay(3000);
+                }
+            }
+        }
+
+        private static void Print(string message, ConsoleColor color)
+        {
+            lock (_lock)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
                 Console.ResetColor();
-
-                await feedbackReceiver.CompleteAsync(feedbackBatch);
             }
         }
     }
