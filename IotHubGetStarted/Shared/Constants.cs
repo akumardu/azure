48b299a8569784e08df9b2c887fe55824fadb546
Amar Kumar Dubedy <amdubedy@microsoft.com>
2018-06-19 21:38:46 -0700

    Working project

--- IotHubGetStarted/Shared/Constants.cs
+++ IotHubGetStarted/Shared/Constants.cs
@@ -8,14 +8,14 @@ namespace Shared
 {
     public static class Constants
     {
-        public static string IotHubConnectionString = "HostName=amar-rp-split-3.private.azure-devices-int.net;SharedAccessKeyName=iothubowner;SharedAccessKey=6YLitcJRO9knpoKl0uBOJY9SsnFFGy23uqxqBbHfFIA=";
+        public static string IotHubConnectionString = "HostName=testiothubs2.private.azure-devices-int.net;SharedAccessKeyName=iothubowner;SharedAccessKey=fDsx6/mifi+EBYrcUwVn5XekLxqZKrpd/7Rua50yxZk=";
 
-        public static string IotHubUri = "amar-rp-split-3.private.azure-devices-int.net";
+        public static string IotHubUri = "testiothubs2.private.azure-devices-int.net";
 
         public static string DeviceId1 = "myFirstDevice";
         public static string DeviceId2 = "mySecondDevice";
 
-        public static string DeviceId1Key = "OsjBDuKWyem4BTOFixMFKK6s8Q3L71urJnB7Yvi4nFk=";
-        public static string DeviceId2Key = "3h3PiEVGeaowmkKCe0NQnhkDE0L/Y87bzmvQwf4uIw4=";
+        public static string DeviceId1Key = "cYA31TVBQBfYXV9l0TCHKSsZDA1dLmz/9W0vI7B0xGw=";
+        public static string DeviceId2Key = "/HIowoQj3mZaCPRNNSLT7Bsv7JOuCALSN+4kn7cH/vY=";
     }
 }
