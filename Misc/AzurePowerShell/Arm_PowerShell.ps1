8154f630e492343a80a60f7c2e044e75aa86e591
Amar Kumar Dubedy <amarkrdubedy@gmail.com>
2018-09-10 20:02:30 -0700

    Rearraging files

---
+++ Misc/AzurePowerShell/Arm_PowerShell.ps1
@@ -0,0 +1,21 @@
+﻿Login-AzureRmAccount
+
+# To get which resource providers support which resources use
+Get-AzureRmResourceProvider | Select-Object ProviderNamespace, ResourceTypes | Sort-Object ProviderNamespace
+
+# To get all locations on which resources exist
+Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Devices| Select-Object ResourceTypes, Locations | Sort-Object ResourceTypes
+
+# REST command for above
+# https://management.azure.com/subscriptions/{subscriptionid}/providers/{provider-name}?&api-version={api-version}
+
+# To get all available API versions for a particular resource type
+((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Devices).ResourceTypes | Where-Object {$_.ResourceTypeName -eq 'IotHubs'}).ApiVersions
+
+# To get all resource groups
+Get-AzureRmResourceGroup
+
+# To get all resources
+Get-AzureRmResource
+
+
